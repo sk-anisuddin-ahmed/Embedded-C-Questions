ad446d79c5eb913592eda7209fd47485adc6563f
Sk Anisuddin Ahmed <skanisuddinahmed@gmail.com>
2025-11-11 15:51:20 +0530

    .

--- 01 - Arrays/002_rearrange_alternate_pos_neg.c
+++ 01 - Arrays/002_rearrange_alternate_pos_neg.c
@@ -1,6 +1,7 @@
 /*
 2. Rearrange array elements so that positive and negative numbers are placed alternatively
-Without changing the relative order of positive and negative numbers, rearrange them alternately. Extra space should be avoided if possible.
+Without changing the relative order of positive and negative numbers, rearrange them alternately. 
+Extra space should be avoided if possible.
 
 Example: Input: [1, 2, -3, -1, -2, 3]
 Output: [1, -3, 2, -1, 3, -2]
